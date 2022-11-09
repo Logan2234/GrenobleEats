07d2b8a61d0ec0462fced7db5692d85ab7b3b2b8
Jorge Luri Vañó <jorge.luri@redarquiadigital.com>
2022-11-09 11:10:44 +0100

    Connexino + création de table + fermeture + destruction de table

--- Interpréteur/ProjetBDD/DriverJDBC.java
+++ Interpréteur/ProjetBDD/DriverJDBC.java
@@ -10,13 +10,12 @@ public class DriverJDBC {
 	
 	public DriverJDBC() {
 		
-		this.url = " jdbc: oracle : thin : @oracle1 . ensimag . fr : 1521:oracle1 ";
+		this.url = "jdbc:oracle:thin:@oracle1.ensimag.fr:1521:oracle1";
 		this.user = "lurivanj";
-		this.passwrd =  "lurivanj ";
+		this.passwrd =  "lurivanj";
 	
 		try { 
-			DriverManager.registerDriver(
-			new oracle.jdbc.driver.OracleDriver() );
+			DriverManager.registerDriver( new oracle.jdbc.driver.OracleDriver() );
 			System.out.println(" -- -- -- \n Création du Driver Oracle \n -- -- -- \n"); 
 		
 		} catch (SQLException e) {
@@ -27,10 +26,11 @@ public class DriverJDBC {
 	}
 	
 	public void connexion() {
+		
 		try {
-			
+			System.out.println(" -- -- -- \n Tentative de connexion \n"); 
 			this.connection = DriverManager.getConnection(this.url, this.user, this.passwrd);
-			System.out.println(" -- -- -- \n Connexion établie \n -- -- -- \n");
+			System.out.println(" Connexion établie \n -- -- -- \n");
 			
 		} catch (SQLException e) { 
 				
@@ -39,10 +39,34 @@ public class DriverJDBC {
 		}
 	}
 	
-	public void creationTable() {
+	public void creationTable(String nomTable) {
 		try {
+			System.out.println(" -- -- -- \n Création de table : " + nomTable + " \n -- -- -- \n");
 			Statement stmt = this.connection.createStatement();
-			stmt.executeQuery("CREATE TABLE Test(hey integer, PRIMARY KEY(hey));");
+			stmt.executeUpdate("CREATE TABLE " + nomTable + "(hey integer)");
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void effacerTable(String nomTable) {
+		try {
+			System.out.println(" -- -- -- \n Effaçage de table : " + nomTable + " \n -- -- -- \n");
+			Statement stmt = this.connection.createStatement();
+			stmt.executeUpdate("DROP TABLE " + nomTable);
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void fermeture() {
+		System.out.println(" -- -- -- \n Tentative de fermeture de connexion \n -- -- -- \n");
+		try {
+			this.connection.close();
 			
 		} catch (SQLException e) {
 			
--- Interpréteur/ProjetBDD/Test.java
+++ Interpréteur/ProjetBDD/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		DriverJDBC test = new DriverJDBC();
 		test.connexion();
-		test.creationTable();
-		System.out.println("Table créée"); 
+		test.creationTable("Test1");
+		test.fermeture();
 	}
 }
